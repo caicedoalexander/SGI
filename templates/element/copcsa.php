60c3c04627dd934c66354d5f545828c6aa26f5ef
Alexander <caicedoficial@gmail.com>
2026-02-19 16:41:06 -0500

    Mejorar estilo del elemento COPCSA en la plantilla copcsa.php

--- templates/element/copcsa.php
+++ templates/element/copcsa.php
@@ -1,3 +1,3 @@
-<div class="position-fixed end-0 bottom-0 m-2 bg-white shadow rounded-pill" style="z-index: 1100;">
-    <img src="<?= $this->Url->build('img/copcsa.png') ?>" alt="COPCSA" class="img-fluid" width="52">
+<div class="position-fixed end-0 bottom-0 m-2 bg-white shadow rounded-pill p-1" style="z-index: 1100; width: 52px; height: 52px;">
+    <img src="<?= $this->Url->build('img/copcsa.png') ?>" alt="COPCSA" class="img-fluid w-100 h-100 object-fit-contain p-1">
 </div>
